11cf8651d04ad940a12859bda764735348037d78
Ahmadnhy <begopeoplee@gmail.com>
2025-06-16 05:51:22 +0700

    ganti title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class LexiconApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lexicon 1',
+      title: 'Lexicon',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
